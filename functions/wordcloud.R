a3fddac9089eca5c91ac906fb1c1da9ad6d02f8d
Anuj Desai <anujkdesai@gmail.com>
2016-08-05 15:21:31 -0700

    layout

--- functions/wordcloud.R
+++ functions/wordcloud.R
@@ -1,6 +1,12 @@
+# Library
 library(wordcloud)
 
-word_cloud <- function(words, frequency, colors) {
+# Functions:
+# Creates word cloud for vector of words, sized and colored by frequency
+# Args: words - vector of words 
+#       frequency - # of reviews word appears in
+#       colors - color scale for word cloud
+WordCloud <- function(words, frequency, colors) {
   wordcloud(words, frequency, scale = c(4, 0.5),
             random.order = F, random.color = F, use.r.layout = T,
             rot.per = 0.35, colors = colors)
