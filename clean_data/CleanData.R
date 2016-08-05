0d7bc04476392da9f2659939a5ad4d9dc7ee2726
Anuj Desai <anujkdesai@gmail.com>
2016-08-04 21:13:01 -0700

    clean_data

--- clean_data/CleanData.R
+++ clean_data/CleanData.R
@@ -4,12 +4,11 @@ library(readr)
 
 # Read in raw data convert to data frames
 business <- fromJSON(sprintf("[%s]", 
-                             paste(read_lines("../raw_data/yelp_academic_dataset_business.json"), 
-                                   collapse = ","))) %>% 
+                     paste(read_lines(
+                       "../raw_data/yelp_academic_dataset_business.json"), 
+                     collapse = ","))) %>% 
   flatten() %>% 
-<<<<<<< HEAD
-  data.frame()
-=======
+  data.frame() %>% 
   select(business_id, open, categories, review_count, state, stars, 
          `attributes.Noise Level`, `attributes.Attire`, `attributes.Take-out`,
          `attributes.Takes Reservations`, `attributes.Delivery`,
@@ -17,7 +16,6 @@ business <- fromJSON(sprintf("[%s]",
          `attributes.Happy Hour`) %>% 
   filter(open == TRUE, "Restaurants" %in% categories, state %in% state.abb)
 business <- data.frame(lapply(business, as.character), stringsAsFactors=FALSE)
->>>>>>> f2c09d1744a6671f9355ee7b97639d4970a94b7e
 
 reviews <- fromJSON(sprintf("[%s]", 
                     paste(read_lines(
