ff664730bf4ba665766313f2f8d7e1b836c95c61
Anuj Desai <anujkdesai@gmail.com>
2016-08-07 13:32:36 -0700

    sizing

--- functions/wordcloud.R
+++ functions/wordcloud.R
@@ -7,7 +7,7 @@ library(wordcloud)
 #       frequency - # of reviews word appears in
 #       colors - color scale for word cloud
 WordCloud <- function(words, frequency, colors) {
-  wordcloud(words, frequency, scale = c(4, 0.5),
+  wordcloud(words, frequency, scale = c(2.75, 0.5),
             random.order = F, random.color = F, use.r.layout = T,
             rot.per = 0.35, colors = colors)
 }
