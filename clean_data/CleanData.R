6c6da920348fbdd852491aa5da91af4056a849ee
Anuj Desai <anujkdesai@gmail.com>
2016-08-08 19:12:52 -0700

    cleaning

--- clean_data/CleanData.R
+++ clean_data/CleanData.R
@@ -42,3 +42,44 @@ users <- data.frame(lapply(users, as.character), stringsAsFactors=FALSE)
 write_csv(business, "yelp_business.csv")
 write_csv(reviews, "yelp_reviews.csv")
 write_csv(users, "yelp_users.csv")
+
+# ------------------- Mutating Data for Sentiment Graphs Below -----------------------
+review_sample <- read_csv("yelp_reviews.csv") %>% 
+  sample_n(500000)
+reviews_text <- review_sample %>% 
+  select(business_id, review_id, stars, text) %>%
+  unnest_tokens(word, text) %>% 
+  anti_join(stop_words) %>% 
+  arrange(review_id)
+
+write_csv(reviews_text, "reviews_text.csv")
+
+# rank words from -5 (negativity) to 5 (positivity) using AFINN lexicon
+# sentiments: 
+#   data frame with 4 variables: word, sentiment, lexicon, score
+# lexicon can be "nrc", "bing", or "AFINN"
+valence_score <- sentiments %>% 
+  filter(lexicon == "AFINN") %>% 
+  select(word, score)
+
+# calculate mean for each review
+# Tidied: review_id, star rating, average sentiment score
+review_scores <- reviews_text %>% 
+  inner_join(valence_score, by = "word") %>% 
+  group_by(review_id, stars) %>% 
+  summarise(sentiment = mean(score))
+write_csv(review_scores, "review_scores.csv")
+
+review_words <- reviews_text %>% 
+  count(review_id, business_id, stars, word) %>% 
+  ungroup()
+summaries <- review_words %>% 
+  group_by(word) %>% 
+  summarise(business = n_distinct(business_id),
+            reviews = n(),
+            average_star = mean(stars)) %>% 
+  ungroup() %>% 
+  filter(reviews >= 500, business >= 10) %>% 
+  inner_join(valence_score, by = "word")
+head(summaries, n = 10)
+write_csv(summaries, "summaries.csv")
